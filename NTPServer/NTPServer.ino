eca55f442a51622a9b8e2acf8cf810f830d832af
Harshbir-Singh <harshbir1279@gmail.com>
2025-06-07 19:33:10 +0530

    Add files via upload

---
+++ NTPServer/NTPServer.ino
@@ -0,0 +1,51 @@
+#include <WiFi.h>
+#include "time.h"
+#include "esp_sntp.h"
+
+const char *ssid = "ACT Network";
+const char *password = "13150517";
+
+const char *ntpServer1 = "pool.ntp.org";
+const char *ntpServer2 = "time.nist.gov";
+
+// Timezone for India (UTC+5:30)
+const char *time_zone = "IST-5:30";
+
+void printLocalTime() {
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo)) {
+    Serial.println("No time available (yet)");
+    return;
+  }
+  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+}
+
+void timeavailable(struct timeval *t) {
+  Serial.println("Got time adjustment from NTP!");
+  printLocalTime();
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  Serial.printf("Connecting to %s ", ssid);
+  WiFi.begin(ssid, password);
+
+  esp_sntp_servermode_dhcp(1);  // Optional
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println(" CONNECTED");
+
+  sntp_set_time_sync_notification_cb(timeavailable);
+
+  // Set timezone and NTP servers
+  configTzTime(time_zone, ntpServer1, ntpServer2);
+}
+
+void loop() {
+  delay(5000);
+  printLocalTime();
+}
